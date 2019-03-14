f6dab44fe910a98e8c06c1f6aca3b37d99fd45ef
Ayako-Saito <sonaare-urenashi@infoseek.jp>
2019-03-15 01:04:33 +0900

    仮レイアウト実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-  before_action :color_string, only: [:create, :update]  # createとupdateの前に必ず実行
+  # before_action :box_string, only: [:create]  # createの前に必ず実行
   def index
     @topics = Topic.all
   end
@@ -12,28 +12,6 @@ class TopicsController < ApplicationController
     @topic = Topic.find(params[:id])
   end
 
-  # def topic
-  #   # params:[gamelist]で配列の値を取得可能。
-  #   # 二次元配列の場合、受取先の変数を2つ宣言する。下記例では、
-  #   # di1がタイトル、di2がチェックボックスの状態(0 or 1)が格納される。
-  #   params[:color].each do | di1,di2 |
-  #
-  #     # チェックボックスにチェックがついている場合
-  #     if di2 == "1"
-  #       # DBのtitleカラムにタイトルを格納し保存
-  #       @topic = Topic.new(box:di1)
-  #       @topic.save
-  #     end
-  #   end
-  #   redirect_to("topic_path(topic.id)")
-  # end
-
-  # 配列を文字列にする（/区切り）
-      def color_string
-        params[:order][:color] = params[:order][:color].join("/")  # to string
-      end
-  end
-
 
   def create
     @topic = current_user.topics.new(topic_params)
@@ -44,6 +22,17 @@ class TopicsController < ApplicationController
       flash.now[:danger] = "投稿できませんでした"
       render :new
     end
+  end
+
+    # 配列を文字列にする（/区切り）
+  # def box_string
+  #   params[:topic][:box] = params[:topic][:box].join("/")
+  # end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @topic.destroy
+    redirect_to topics_path, notice: "投稿を削除しました"
   end
 
   private
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,7 @@ class Topic < ApplicationRecord
   validates :user_id, presence: true
   # validates :contents, presence: true  考え中
   validates :image, presence: true
+  validates :box, presence: true  
 
   belongs_to :user
 
