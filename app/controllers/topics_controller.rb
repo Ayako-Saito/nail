a21a2e75601f0a14fce0cc001e607e63a80be1d1
Ayako-Saito <sonaare-urenashi@infoseek.jp>
2019-03-19 20:15:43 +0900

    チェック、セレクトボックスOK

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,33 +1,34 @@
 class TopicsController < ApplicationController
-  # before_action :box_string, only: [:create]  # createの前に必ず実行
+  @@money_list = [["5,000円以下", "1"],["5,000円~10,000円", "2"],["10,000円~15,000円", "3"],["15,000円~20,000円", "4"],["20,000円以上","5"]]
+
   def index
     @topics = Topic.all
   end
 
   def new
     @topic = Topic.new
+    @money_list = @@money_list
   end
 
   def show
     @topic = Topic.find(params[:id])
+    # @unbox = @topic.box.tr("[]\" ","").split(",")
+    @unbox = eval(@topic.box)
+    @money_text = @@money_list.select { |op| op[1] == @topic.money }.flatten(1)[0]
   end
 
 
   def create
     @topic = current_user.topics.new(topic_params)
-
-    if @topic.save
+    p topic_params
+    if @topic.save!
       redirect_to topics_path, success: '投稿しました'
     else
+      logger.debug(@item.errors.full_messages)
       flash.now[:danger] = "投稿できませんでした"
       render :new
     end
   end
-
-    # 配列を文字列にする（/区切り）
-  # def box_string
-  #   params[:topic][:box] = params[:topic][:box].join("/")
-  # end
 
   def destroy
     @topic = Topic.find(params[:topic_id])
@@ -37,6 +38,6 @@ class TopicsController < ApplicationController
 
   private
   def topic_params
-    params.require(:topic).permit(:image, :contents, box:[])
+    params.require(:topic).permit(:image, :contents, :money, box:[])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_173326) do
+ActiveRecord::Schema.define(version: 2019_03_17_075409) do
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_03_13_173326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "box"
+    t.string "money"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
